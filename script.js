c22f394b47f8eee34221972e7f989c21fa20cc0d
Gagayogaa <yoghakurniawan35@gmail.com>
2023-12-21 21:30:30 +0700

    update link redirect

---
+++ script.js
@@ -0,0 +1,35 @@
+function linkMaintance() {
+  alert(
+    "Mohon maaf, link belum tersedia🙏🏻🙏🏻, masih dalam pengembangan, Terima kasih."
+  );
+}
+
+function linkGithub() {
+  window.location.href = "https://github.com/gagayoga";
+}
+
+function linkSosmed() {
+  window.location.href = "https://twitter.com/yogairgii";
+}
+
+function linkYoutube() {
+  window.location.href =
+    "https://www.youtube.com/channel/UCSTkVRR9oJO-3l8UW4M8neg";
+}
+
+function linkInstagram() {
+  window.location.href = "https://instagram.com/yogaairgi";
+}
+
+function linkDiscord() {
+  window.location.href = "";
+}
+
+function linkEmainl() {
+  window.location.href = "https://yoghakurniawan35@gmail.com";
+}
+
+function linkFigma() {
+  window.location.href =
+    "https://www.figma.com/proto/msEPEA4ywubmVmkGLSU1s5/Prototipe-aplikasi-Koperasi-Smakma?type=design&node-id=650-3676&t=Q4BVKVBcWrsPg7tX-1&scaling=min-zoom&page-id=646%3A2793&starting-point-node-id=650%3A3676&show-proto-sidebar=1&mode=design";
+}
